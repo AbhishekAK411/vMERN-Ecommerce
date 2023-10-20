1141f5bf8506c927d86b4be38220bc549a6683fa
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-10-20 14:10:00 +0530

    created product controllers

---
+++ server/controllers/prod.cont.js
@@ -0,0 +1,31 @@
+export const addProduct = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({status: 500, success: false, message: "Internal server error."});
+    }
+}
+
+export const editProduct = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({status: 500, success: false, message: "Internal server error."});
+    }
+}
+
+export const getProduct = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({status: 500, success: false, message: "Internal server error."});
+    }
+}
+
+export const deleteProduct = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({status: 500, success: false, message: "Internal server error."});
+    }
+}
